41c18df3ecf341726d4f5361ed402abe9307c64e
Abdo Tareq <abdotarekfathy@gmail.com>
2020-08-26 00:12:33 +0200

    feat: Add fonts & theme

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -8,30 +8,33 @@ class ProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridTile(
-      child: Image.network(
-        product.imageUrl,
-        fit: BoxFit.cover,
-      ),
-      footer: GridTileBar(
-        backgroundColor: Colors.black54,
-        leading: IconButton(
-          icon: Icon(
-            Icons.favorite,
-            color: Colors.white,
-          ),
-          onPressed: () {},
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        child: Image.network(
+          product.imageUrl,
+          fit: BoxFit.cover,
         ),
-        trailing: IconButton(
-          icon: Icon(
-            Icons.shopping_cart,
-            color: Colors.white,
+        footer: GridTileBar(
+          backgroundColor: Colors.black87,
+          leading: IconButton(
+            icon: Icon(
+              Icons.favorite,
+              color: Theme.of(context).accentColor,
+            ),
+            onPressed: () {},
           ),
-          onPressed: () {},
-        ),
-        title: Text(
-          product.title,
-          textAlign: TextAlign.center,
+          trailing: IconButton(
+            icon: Icon(
+              Icons.shopping_cart,
+              color: Theme.of(context).accentColor,
+            ),
+            onPressed: () {},
+          ),
+          title: Text(
+            product.title,
+            textAlign: TextAlign.center,
+          ),
         ),
       ),
     );
