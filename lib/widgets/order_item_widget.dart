46a44bfd0b43290a65427c8bc08cd1ac24b404a5
Abdo Tareq <abdotarekfathy@gmail.com>
2020-08-28 14:41:09 +0200

    feat: Expand order details

--- lib/widgets/order_item_widget.dart
+++ lib/widgets/order_item_widget.dart
@@ -1,23 +1,71 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:shop_app/models/cart_item.dart';
 import 'package:shop_app/models/order_item.dart';
 
-class OrderItemWidget extends StatelessWidget {
+class OrderItemWidget extends StatefulWidget {
   final OrderItem orderItem;
 
   OrderItemWidget(this.orderItem);
 
   @override
+  _OrderItemWidgetState createState() => _OrderItemWidgetState();
+}
+
+class _OrderItemWidgetState extends State<OrderItemWidget> {
+  var _expanded = false;
+
+  @override
   Widget build(BuildContext context) {
+    List<CartItem> products = widget.orderItem.productsList;
     return Card(
       margin: EdgeInsets.all(10),
-      child: ListTile(
-        title: Text('\$${orderItem.amount}'),
-        subtitle: Text('${DateFormat().format(orderItem.dateTime)}'),
-        trailing: IconButton(
-          icon: Icon(Icons.expand_more),
-          onPressed: () {},
-        ),
+      child: Column(
+        children: [
+          ListTile(
+            title: Text('\$${widget.orderItem.amount}'),
+            subtitle: Text('${DateFormat().format(widget.orderItem.dateTime)}'),
+            trailing: IconButton(
+              icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
+              onPressed: () {
+                setState(() {
+                  _expanded = !_expanded;
+                });
+              },
+            ),
+          ),
+          if (_expanded)
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 4, horizontal: 15),
+              height: min(180, products.length * 20.0 + 15),
+              child: ListView.builder(
+                itemCount: products.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        '${products[index].title}',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                        ),
+                      ),
+                      Text(
+                        '${products[index].quantity}x \$${products[index].price.toStringAsFixed(2)}',
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ],
+                  );
+                },
+              ),
+            ),
+        ],
       ),
     );
   }
