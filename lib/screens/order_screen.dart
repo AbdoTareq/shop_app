0a0cec3d8d49a9b236a921fd0959e273a8a22399
Abdo Tareq <abdotarekfathy@gmail.com>
2020-09-01 16:15:07 +0200

    feat: fetch orders from server using future builder

--- lib/screens/order_screen.dart
+++ lib/screens/order_screen.dart
@@ -4,53 +4,49 @@ import 'package:shop_app/providers/order_provider.dart';
 import 'package:shop_app/widgets/app_drawer.dart';
 import 'package:shop_app/widgets/order_item_widget.dart';
 
-class OrderScreen extends StatefulWidget {
+class OrderScreen extends StatelessWidget {
   static const routeName = '/orders';
 
   @override
-  _OrderScreenState createState() => _OrderScreenState();
-}
-
-class _OrderScreenState extends State<OrderScreen> {
-  bool _isLoading = false;
-
-  @override
-  void initState() {
-    super.initState();
-    setState(() {
-      _isLoading = true;
-    });
-    Provider.of<OrderProvider>(context, listen: false)
-        .fetchAndSetOrders()
-        .then((_) {
-      setState(() {
-        _isLoading = false;
-      });
-    }).catchError((e) {
-      print('dart mess: $e');
-      setState(() {
-        _isLoading = false;
-      });
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
-    final orders = Provider.of<OrderProvider>(context).orders;
+    // make infinite loop as it's listening to any changes then rebuild the widget
+//    final orders = Provider.of<OrderProvider>(context).orders;
     return Scaffold(
       appBar: AppBar(
         title: Text('Place Order'),
       ),
-      body: _isLoading
-          ? Center(
+      // this is a better replacement to a state full widget with loading variable
+      body: FutureBuilder(
+        // this provider will get the data then notifyListener which will build the widget then final provider will be created
+        future: Provider.of<OrderProvider>(context, listen: false)
+            .fetchAndSetOrders(),
+        builder: (BuildContext ctx, AsyncSnapshot<dynamic> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
               child: CircularProgressIndicator(),
-            )
-          : ListView.builder(
-              itemCount: orders.length,
-              itemBuilder: (BuildContext context, int index) {
-                return OrderItemWidget(orders[index]);
+            );
+          } else if (snapshot.error != null) {
+            return Center(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text('Error occurred, \n ${snapshot.error}'),
+              ),
+            );
+          } else {
+            // we used consumer down here to stop infinite loop building
+            return Consumer<OrderProvider>(
+              builder: (BuildContext context, ordersData, Widget child) {
+                return ListView.builder(
+                  itemCount: ordersData.orders.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return OrderItemWidget(ordersData.orders[index]);
+                  },
+                );
               },
-            ),
+            );
+          }
+        },
+      ),
       drawer: AppDrawer(),
     );
   }
