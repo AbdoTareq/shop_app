56cdd46271f291cc13f47255743328b778fd90e6
Abdo Tareq <abdotarekfathy@gmail.com>
2020-09-06 14:45:23 +0200

    feat: Add animated container to orders items

--- lib/widgets/order_item_widget.dart
+++ lib/widgets/order_item_widget.dart
@@ -20,52 +20,59 @@ class _OrderItemWidgetState extends State<OrderItemWidget> {
   @override
   Widget build(BuildContext context) {
     List<CartItem> products = widget.orderItem.productsList;
-    return Card(
-      margin: EdgeInsets.all(10),
-      child: Column(
-        children: [
-          ListTile(
-            title: Text('\$${widget.orderItem.amount}'),
-            subtitle: Text('${DateFormat().format(widget.orderItem.dateTime)}'),
-            trailing: IconButton(
-              icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
-              onPressed: () {
-                setState(() {
-                  _expanded = !_expanded;
-                });
-              },
-            ),
-          ),
-          if (_expanded)
-            Container(
-              margin: EdgeInsets.symmetric(vertical: 4, horizontal: 15),
-              height: min(180, products.length * 20.0 + 15),
-              child: ListView.builder(
-                itemCount: products.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        '${products[index].title}',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18,
-                        ),
-                      ),
-                      Text(
-                        '${products[index].quantity}x \$${products[index].price.toStringAsFixed(2)}',
-                        style: TextStyle(
-                          fontSize: 18,
-                          color: Colors.grey,
-                        ),
-                      ),
-                    ],
-                  );
+    return AnimatedContainer(
+      duration: Duration(milliseconds: 350),
+      curve: Curves.fastOutSlowIn,
+      height: _expanded ? min(280, products.length * 20.0 + 115) : 100,
+      child: Card(
+        margin: EdgeInsets.all(10),
+        child: Column(
+          children: [
+            ListTile(
+              title: Text('\$${widget.orderItem.amount}'),
+              subtitle:
+                  Text('${DateFormat().format(widget.orderItem.dateTime)}'),
+              trailing: IconButton(
+                icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
+                onPressed: () {
+                  setState(() {
+                    _expanded = !_expanded;
+                  });
                 },
               ),
             ),
-        ],
+              Expanded(
+                child: Container(
+                  margin: EdgeInsets.symmetric(vertical: 4, horizontal: 15),
+                  height: min(180, products.length * 20.0 + 15),
+                  child: ListView.builder(
+                    itemCount: products.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            '${products[index].title}',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18,
+                            ),
+                          ),
+                          Text(
+                            '${products[index].quantity}x \$${products[index].price.toStringAsFixed(2)}',
+                            style: TextStyle(
+                              fontSize: 18,
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ],
+                      );
+                    },
+                  ),
+                ),
+              ),
+          ],
+        ),
       ),
     );
   }
